63b4cf01939d329564aec6dcaf8f20de716a539e
yamashita4140 <yamashita4140@gmail.com>
2022-03-18 17:09:43 +0900

    "Hello Woerld"

---
+++ kihon_01.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>基本</title>
+  </head>
+  <body>
+    <?php
+    print "Hello World!!!";
+     ?>
+
+  </body>
+</html>
